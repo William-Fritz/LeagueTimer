9dca3251f97f6800e1c004fec399bb9467d2ae33
William <prasetyopang29@gmail.com>
2019-03-20 16:40:38 +0700

    added fun on text so it may be clicked and behave the same way as the image button

--- app/src/main/java/com/example/prototype1/MainActivity.kt
+++ app/src/main/java/com/example/prototype1/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.example.prototype1
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
-import android.widget.Toast
-import android.widget.ToggleButton
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -27,6 +27,17 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
+        val top_lane_status2 = findViewById<TextView>(R.id.top_lane_status2)
+        top_lane_status2.setOnClickListener {
+            if (button_check == true) {
+                top_lane_status2.setText(getString(R.string.cooldown))
+                button_check = false
+            } else {
+                top_lane_status2.setText(getString(R.string.ready))
+                button_check = true
+            }
+        }
     }
 }
+
 
