e9b045d4364d7a2ace346491b8b7a51f434da60a
William <prasetyopang29@gmail.com>
2019-03-21 16:05:15 +0700

    changed the button_check var to button_is_clicked to make it more descriptive because it is boolean so it makes more sense.. deleted one line before else statement

--- app/src/main/java/com/example/prototype1/MainActivity.kt
+++ app/src/main/java/com/example/prototype1/MainActivity.kt
@@ -4,10 +4,8 @@ import android.graphics.Color
 import android.graphics.PorterDuff
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.ImageButton
 import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,16 +16,16 @@ class MainActivity : AppCompatActivity() {
 
         //create variables for button check
 
-        var button_check_left_top = true
-        var button_check_left_mid = true
-        var button_check_left_jungle = true
-        var button_check_left_bottom = true
-        var button_check_left_support = true
-        var button_check_right_top = true
-        var button_check_right_mid = true
-        var button_check_right_jungle = true
-        var button_check_right_bottom = true
-        var button_check_right_support = true
+        var left_top_button_is_clicked = true
+        var left_mid_button_is_clicked = true
+        var left_jungle_button_is_clicked = true
+        var left_bottom_button_is_clicked = true
+        var left_support_button_is_clicked = true
+        var right_top_button_is_clicked = true
+        var right_mid_button_is_clicked = true
+        var right_jungle_button_is_clicked = true
+        var right_bottom_button_is_clicked = true
+        var right_support_button_is_clicked = true
 
         //create variables for all imagebutton and textview
 
@@ -54,81 +52,79 @@ class MainActivity : AppCompatActivity() {
 
         //button listener
         right_top_lane_button.setOnClickListener {
-            button_check_right_top = toggle_timer(right_top_lane_status, right_top_lane_button, button_check_right_top)
+            right_top_button_is_clicked = toggle_timer(right_top_lane_status, right_top_lane_button, right_top_button_is_clicked)
         }
         right_top_lane_status.setOnClickListener{
-            button_check_right_top = toggle_timer(right_top_lane_status, right_top_lane_button, button_check_right_top)
+            right_top_button_is_clicked = toggle_timer(right_top_lane_status, right_top_lane_button, right_top_button_is_clicked)
         }
         left_top_lane_button.setOnClickListener {
-            button_check_left_top = toggle_timer(left_top_lane_status, left_top_lane_button, button_check_left_top)
+            left_top_button_is_clicked = toggle_timer(left_top_lane_status, left_top_lane_button, left_top_button_is_clicked)
         }
         left_top_lane_status.setOnClickListener{
-            button_check_left_top = toggle_timer(left_top_lane_status, left_top_lane_button, button_check_left_top)
+            left_top_button_is_clicked = toggle_timer(left_top_lane_status, left_top_lane_button, left_top_button_is_clicked)
         }
         right_jungle_button.setOnClickListener {
-            button_check_right_jungle = toggle_timer(right_jungle_status, right_jungle_button, button_check_right_jungle)
+            right_jungle_button_is_clicked = toggle_timer(right_jungle_status, right_jungle_button, right_jungle_button_is_clicked)
         }
         right_jungle_status.setOnClickListener{
-            button_check_right_jungle = toggle_timer(right_jungle_status, right_jungle_button, button_check_right_jungle)
+            right_jungle_button_is_clicked = toggle_timer(right_jungle_status, right_jungle_button, right_jungle_button_is_clicked)
         }
         left_jungle_button.setOnClickListener {
-            button_check_left_jungle = toggle_timer(left_jungle_status, left_jungle_button, button_check_left_jungle)
+            left_jungle_button_is_clicked = toggle_timer(left_jungle_status, left_jungle_button, left_jungle_button_is_clicked)
         }
         left_jungle_status.setOnClickListener {
-            button_check_left_jungle = toggle_timer(left_jungle_status, left_jungle_button, button_check_left_jungle)
+            left_jungle_button_is_clicked = toggle_timer(left_jungle_status, left_jungle_button, left_jungle_button_is_clicked)
         }
         right_mid_lane_button.setOnClickListener {
-            button_check_right_mid = toggle_timer(right_mid_lane_status, right_mid_lane_button, button_check_right_mid)
+            right_mid_button_is_clicked = toggle_timer(right_mid_lane_status, right_mid_lane_button, right_mid_button_is_clicked)
         }
         right_mid_lane_status.setOnClickListener {
-            button_check_right_mid = toggle_timer(right_mid_lane_status, right_mid_lane_button, button_check_right_mid)
+            right_mid_button_is_clicked = toggle_timer(right_mid_lane_status, right_mid_lane_button, right_mid_button_is_clicked)
         }
         left_mid_lane_button.setOnClickListener {
-            button_check_left_mid = toggle_timer(left_mid_lane_status, left_mid_lane_button, button_check_left_mid)
+            left_mid_button_is_clicked = toggle_timer(left_mid_lane_status, left_mid_lane_button, left_mid_button_is_clicked)
         }
         left_mid_lane_status.setOnClickListener {
-            button_check_left_mid = toggle_timer(left_mid_lane_status, left_mid_lane_button, button_check_left_mid)
+            left_mid_button_is_clicked = toggle_timer(left_mid_lane_status, left_mid_lane_button, left_mid_button_is_clicked)
         }
         right_bottom_lane_button.setOnClickListener {
-            button_check_right_bottom = toggle_timer(right_bottom_lane_status, right_bottom_lane_button, button_check_right_bottom)
+            right_bottom_button_is_clicked = toggle_timer(right_bottom_lane_status, right_bottom_lane_button, right_bottom_button_is_clicked)
         }
         right_bottom_lane_status.setOnClickListener {
-            button_check_right_bottom = toggle_timer(right_bottom_lane_status, right_bottom_lane_button, button_check_right_bottom)
+            right_bottom_button_is_clicked = toggle_timer(right_bottom_lane_status, right_bottom_lane_button, right_bottom_button_is_clicked)
         }
         left_bottom_lane_button.setOnClickListener {
-            button_check_left_bottom = toggle_timer(left_bottom_lane_status, left_bottom_lane_button, button_check_left_bottom)
+            left_bottom_button_is_clicked = toggle_timer(left_bottom_lane_status, left_bottom_lane_button, left_bottom_button_is_clicked)
         }
         left_bottom_lane_status.setOnClickListener {
-            button_check_left_bottom = toggle_timer(left_bottom_lane_status, left_bottom_lane_button, button_check_left_bottom)
+            left_bottom_button_is_clicked = toggle_timer(left_bottom_lane_status, left_bottom_lane_button, left_bottom_button_is_clicked)
         }
         right_support_button.setOnClickListener {
-            button_check_right_support = toggle_timer(right_support_status, right_support_button, button_check_right_support)
+            right_support_button_is_clicked = toggle_timer(right_support_status, right_support_button, right_support_button_is_clicked)
         }
         right_support_status.setOnClickListener {
-            button_check_right_support = toggle_timer(right_support_status, right_support_button, button_check_right_support)
+            right_support_button_is_clicked = toggle_timer(right_support_status, right_support_button, right_support_button_is_clicked)
         }
         left_support_button.setOnClickListener {
-            button_check_left_support = toggle_timer(left_support_status, left_support_button, button_check_left_support)
+            left_support_button_is_clicked = toggle_timer(left_support_status, left_support_button, left_support_button_is_clicked)
         }
         left_support_status.setOnClickListener {
-            button_check_left_support = toggle_timer(left_support_status, left_support_button, button_check_left_support)
+            left_support_button_is_clicked = toggle_timer(left_support_status, left_support_button, left_support_button_is_clicked)
         }
     }
 
     //function to change the text and the foreground color of button
 
-    fun toggle_timer(text: TextView, imageButton: ImageButton, button_check: Boolean): Boolean {
-        var toggle_active = button_check
+    fun toggle_timer(text: TextView, imageButton: ImageButton, button_is_clicked: Boolean): Boolean {
+        var toggle_active = button_is_clicked
         if (toggle_active) {
             text.setText(getString(R.string.cooldown))
             imageButton.setColorFilter(Color.DKGRAY, PorterDuff.Mode.MULTIPLY)
-        }
-        else {
+        } else {
             text.setText(getString(R.string.ready))
             imageButton.setColorFilter(null)
         }
         toggle_active = !toggle_active
-
         return toggle_active
     }
 }
