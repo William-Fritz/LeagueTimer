fe2ddc1265c6fc1edab29b7962e2d298a345222c
Prasetyo Pangestu <47554645+William-Fritz@users.noreply.github.com>
2019-04-05 13:22:05 +0700

    Welcome screen and icon (#9)
    * added welcome screen and icon file
    
    * added welcome screen and icon logo in resources
    
    * changed the hardcoded color on text

---
+++ app/src/main/java/com/example/prototype1/WelcomeActivity.kt
@@ -0,0 +1,25 @@
+package com.example.prototype1
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
+
+class WelcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.requestFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_welcome)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        },3000)
+
+
+    }
+}
